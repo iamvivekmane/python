71b52078f5ccded8f98fe5343e482fa4e6059864
Vivek Mane <vivekmane172@gmail.com>
2026-09-22 08:36:42 +0530

    arithmetic, assignment, comparison, logical operators in python

---
+++ Day 01/operators.py
@@ -0,0 +1,33 @@
+a = 10
+b = 20
+
+# Arithmetic operator
+c = a + b
+
+# Assignment operator
+d = 5-2
+c +=3
+
+# Comparison operator
+print(a==b)
+print(a<=b)
+print(a>=b)
+print(c)
+
+# Logical operators
+# or
+print('True or False is',True or False)
+print('True or True is',True or True)
+print('False or True is',False or True)
+print('False or False is',False or False)
+
+# and
+print('True and False is',True and False)
+print('True and True is',True and True)
+print('False and True is',False and True)
+print('False and False is',False and False)
+
+# not
+print('Not of true is',not(True))
+print('Not of true is',not(False))
+
