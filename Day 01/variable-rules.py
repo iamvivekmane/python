86c75159b6e1bf3f458a576f514f545bd81bfca6
Vivek Mane <vivekmane172@gmail.com>
2026-09-22 08:05:32 +0530

    rules for declaring a variable

---
+++ Day 01/variable-rules.py
@@ -0,0 +1,7 @@
+# 1. A variable name can contain alphabets, digits and underscores.
+
+# 2. A variable name can only start with alphabet or underscore.
+
+# 3. A variable name cant start with digits.
+
+# 4. No whitespaces are allowed to be inside the name of the variable.
