f92fe52ff220149afd67912280321dc27abc1155
Vivek Mane <vivekmane172@gmail.com>
2026-09-22 16:00:20 +0530

    escape sequence characters in python

---
+++ Day 01/escape_sequence.py
@@ -0,0 +1,11 @@
+# it adds a new line for the letters after it
+a = "this is the string that\ni am talking about yesterday"
+print(a)
+
+# it adds a tab space 
+b = "this is the string that \ti am talking about yesterday"
+print(b) 
+
+# it lets us add quotes in the string without getting an error 
+b = "this is the string that \"i\" am talking about yesterday"
+print(b) 
