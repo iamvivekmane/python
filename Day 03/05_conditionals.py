8f4dda3ca68d6da51359ac2e6e9b7b2ca6e38eb4
Vivek Mane <vivekmane172@gmail.com>
2026-09-24 18:14:12 +0530

    if else statement

---
+++ Day 03/05_conditionals.py
@@ -0,0 +1,9 @@
+# if else statement
+number = int(input("Enter any number : "))
+if(number>=0):
+    print("The number is positive")
+else:
+    print("The number is negative")
+
+age = int(input("Enter your age : "))
+
