fa223d36aa2f3b3bbb7bcfc6f6f387812b5b1b90
Vivek Mane <vivekmane172@gmail.com>
2026-09-24 18:14:34 +0530

    if else if ladder

--- Day 03/05_conditionals.py
+++ Day 03/05_conditionals.py
@@ -7,3 +7,11 @@ else:
 
 age = int(input("Enter your age : "))
 
+if(age>18):
+    print("You are able to drive")
+elif(age==0):
+    print("You are just born")
+elif(age<0):
+    print("Age can't be negative")
+else:
+    print("You are not able to drive")
